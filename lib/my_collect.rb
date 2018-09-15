cd3ea26bcc38ce6f6f11be3b5b4ab0aad40e1652
Nkosi Phillip <internap627@gmail.com>
2018-09-15 06:34:23 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -4,10 +4,10 @@ def my_collect(array)
   i = 0
   newArray = []
   while i < array.length
-  yield array[i]
+  newArray[i] = yield array[i]
   newArray.push(value)
   i += 1
   end
 
-  
+  newArray
 end
